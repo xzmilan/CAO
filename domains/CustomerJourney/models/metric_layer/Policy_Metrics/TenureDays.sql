2e81e69bd0d9d9b0e34f1dea679e69a5459ec042
Steven Passanante <xzmilan@gmail.com>
2026-08-10 09:28:34 -0400

    test: verify two-workflow split

--- domains/CustomerJourney/models/metric_layer/Policy_Metrics/TenureDays.sql
+++ domains/CustomerJourney/models/metric_layer/Policy_Metrics/TenureDays.sql
@@ -9,3 +9,5 @@ SELECT
     Policy.ID
     , DATEDIFF('day', Policy.Policy:PolicyInceptionDate, CURRENT_DATE()) AS TenureDays
 FROM {{ ref('PolicyRaw') }} AS Policy
+
+-- split workflow verification touch (safe to revert)
